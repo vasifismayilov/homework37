04b857d42d406cd2d6be9a9e6dedd0a4f0811dad
vasifismayilov <ismayilov2265@gmail.com>
2022-06-09 16:27:51 +0400

    Homework37

---
+++ homework38.py
@@ -0,0 +1,12 @@
+#1. Geri sayim programi hazirlayin.
+#   Input olaraq bir deyer verin və program həmin dəyərdən 0-da doğru saniyə-saniyə geri saysın. 0-a çatdıqda bitdi desin.
+
+# import time
+# 
+# geri_sayim = int(input('Geri sayimin baslamasi ucun bir eded daxil edin:' ))
+# 
+# for i in range(geri_sayim,0,-1):
+#     print(i)
+#     time.sleep(0.2)
+# 
+# print('Geri sayim bitmisdir!!!')    
